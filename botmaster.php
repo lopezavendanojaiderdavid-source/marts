1f252da7db55d8d6eafdf6d1fd3b06d6dbfd856d
lopezavendanojaiderdavid-source <lopezavendanojaiderdavid@gmail.com>
2026-03-19 17:58:50 -0500

    Add files via upload

---
+++ botmaster.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'token' => '7567850352:AAE5sck2NClVAc4A0whN2enVaa9HRNPK9Qo',
+    'chat_id' => '-1002951349377'
+];
+?>
---
+++ botmaster2.php
@@ -0,0 +1,8 @@
+<?php
+$config = [
+    'token' => '7567850352:AAE5sck2NClVAc4A0whN2enVaa9HRNPK9Qo',
+    'chat_id' => '-1002951349377'
+];
+header('Content-Type: application/json');
+echo json_encode($config);
+?>
